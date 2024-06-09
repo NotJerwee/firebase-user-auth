47247ff17d7d9aa78a0b75f996c75c854939d61a
Jerry Lin <103540592+NotJerwee@users.noreply.github.com>
2024-06-09 17:23:48 -0400

    Added google authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,11 @@ import { useState } from "react";
 import { Navigate, Link } from "react-router-dom";
 
 // Firebase auth
-import { auth } from "../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, googleProvider } from "../firebase";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
+// Image
+import googleLogoImage from '../assets/googleLogo.png';
 
 export const Login = ({ user }) => {
     const [email, setEmail] = useState("");
@@ -15,6 +18,16 @@ export const Login = ({ user }) => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 console.log(userCredential.user);
+            })
+            .catch((error) => {
+                console.log(error.code, error.message);
+            });
+    };
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                console.log(result.user);
             })
             .catch((error) => {
                 console.log(error.code, error.message);
@@ -56,7 +69,23 @@ export const Login = ({ user }) => {
                             type="submit" 
                             className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                         >
-                            Sign In
+                            Login
+                        </button>
+                    </div>
+                    </div>
+                    <div className="relative flex py-5 items-center">
+                        <div className="flex-grow border-t border-gray-300"></div>
+                        <span className="flex-shrink mx-4 text-gray-500">or</span>
+                        <div className="flex-grow border-t border-gray-300"></div>
+                    </div>
+                    <div>
+                        <button 
+                            type="button" 
+                            onClick={handleGoogleSignIn} 
+                            className="w-full flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                        >
+                            <img src={googleLogoImage} alt="Google Logo" className="w-6 h-6 mr-2" />
+                            Continue with Google
                         </button>
                     </div>
                     <p className="text-sm text-center">
