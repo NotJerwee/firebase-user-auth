6c858273b1a9365b8133056b49bc974c1f14a47f
Jerry <Jerrylin2488@gmail.com>
2024-06-08 23:33:17 -0400

    Added tailwind CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Welcome user!</h1>
-            <p>Homepage</p>
-            <div>
-                <Link to="/login">
-                    <button style={{ margin: '10px', padding: '10px 20px' }}>Sign In</button>
-                </Link>
-                <Link to="/signup">
-                    <button style={{ margin: '10px', padding: '10px 20px' }}>Sign Up</button>
-                </Link>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="text-center">
+                <h1>Welcome user!</h1>
+                <p>Homepage</p>
+                <div>
+                    <Link to="/login">
+                        <button className="m-2 px-4 py-2 bg-blue-500 text-white rounded">Sign In</button>
+                    </Link>
+                    <Link to="/signup">
+                        <button className="m-2 px-4 py-2 bg-blue-500 text-white rounded">Sign Up</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
